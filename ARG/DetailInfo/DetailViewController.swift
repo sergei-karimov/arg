3550e12b427548d2595c39b07fcbcc9ee9e1f29c
sergei rkarimov <s.karimov@outlook.com>
2019-05-04 00:50:22 +0300

    Photos panel was done

--- ARG/DetailInfo/DetailViewController.swift
+++ ARG/DetailInfo/DetailViewController.swift
@@ -8,31 +8,114 @@
 
 import UIKit
 
-class DetailViewController: UIViewController {
+class DetailViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    let chashaImages: [UIImage] = [
+        UIImage(named: "карусель1")!,
+        UIImage(named: "карусель2")!,
+        UIImage(named: "карусель3")!,
+        UIImage(named: "карусель4")!
+    ]
+    
     @IBOutlet weak var infoButton: UIButton!
     @IBOutlet weak var photosButton: UIButton!
     @IBOutlet weak var nightViewButton: UIButton!
     @IBOutlet weak var historyButton: UIButton!
     @IBOutlet weak var ticketButton: UIButton!
+    
     @IBOutlet weak var infoPanel: UIView!
-    @IBOutlet weak var heartButton: UIButton!
+    @IBOutlet weak var photosPanel: UIView!
+
+    @IBOutlet weak var infoHeartButton: UIButton!
+    @IBOutlet weak var photosHeartButton: UIButton!
     
-    var heartState = false
-    
+    var infoHeartState = false
+    var photosHeartState = false
+    var nightViewHeartState = false
+    var hestoryHeartState = false
+    var ticketHeartState = false
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
         // Do any additional setup after loading the view.
-        heartState = false
+        infoHeartState = false
+        photosHeartState = false
+        nightViewHeartState = false
+        hestoryHeartState = false
+        ticketHeartState = false
+
+        let downSwipe = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipes(_:)))
+        downSwipe.direction = .down
+        
+        infoPanel.addGestureRecognizer(downSwipe)
+        photosPanel.addGestureRecognizer(downSwipe)
+        
+        hideAll()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return chashaImages.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CellID", for: indexPath) as! CollectionViewCell
+        cell.photoItem.image = chashaImages[indexPath.item]
+        
+        return cell
+    }
+    
+    func hideAll() {
+        infoPanel.isHidden = true
+        photosPanel.isHidden = true
+    }
+    
+    @IBAction func infoButtonTouchUpInside(_ sender: UIButton) {
+        hideAll()
+        infoPanel.isHidden = false
+    }
+    
+    @IBAction func photosButtonTouchUpInside(_ sender: UIButton) {
+        hideAll()
+        photosPanel.isHidden = false
+    }
+    
+    @objc func handleSwipes(_ sender: UISwipeGestureRecognizer) {
+        if sender.state == .ended {
+            switch sender.direction {
+            case .down:
+                debugPrint("down")
+                dismiss(animated: true)
+                //            case .left:
+            //                debugPrint("left")
+            default:
+                debugPrint("default")
+            }
+        }
     }
     
     @IBAction func heartTouchUpInside(_ sender: Any) {
-        if heartState == false {
-            heartButton.setImage(UIImage(named: "heart-active.png"), for: .normal)
-            heartState = true
+        if infoHeartState == false {
+            infoHeartButton.setImage(UIImage(named: "heart-active.png"), for: .normal)
+            infoHeartState = true
         }else {
-            heartButton.setImage(UIImage(named: "heart.png"), for: .normal)
-            heartState = false
+            infoHeartButton.setImage(UIImage(named: "heart.png"), for: .normal)
+            infoHeartState = false
+        }
+    }
+    
+    @IBAction func photoHeartTouchUpInside(_ sender: UIButton) {
+        if photosHeartState == false {
+            photosHeartButton.setImage(UIImage(named: "heart-active.png"), for: .normal)
+            photosHeartState = true
+        }else {
+            photosHeartButton.setImage(UIImage(named: "heart.png"), for: .normal)
+            photosHeartState = false
         }
     }
 }
